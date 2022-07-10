a3b61fad72c6711dfe3486f9235b1b5afc0df4ca
Matisse <matisse.van.steenbergen@student.howest.be>
2022-07-10 15:57:58 +0200

    finishing template template

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 /* global-css */
-import "./globals/styles/vars.css";
-import "./globals/styles/fonts.css";
-import "./globals/styles/reset.css";
-import "./globals/styles/globals.css";
+import "./globals/styles/vars.scss";
+import "./globals/styles/fonts.scss";
+import "./globals/styles/reset.scss";
+import "./globals/styles/globals.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
