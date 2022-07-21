9a4f1252337e37307932ca7af39e52e0d475afb6
Matisse <matisse.van.steenbergen@student.howest.be>
2022-07-21 20:04:02 +0200

    adding the css of passwordLength

--- src/components/core/passwordGenerator/passwordLength/passwordLength.tsx
+++ src/components/core/passwordGenerator/passwordLength/passwordLength.tsx
@@ -1,24 +1,40 @@
+import React, { useState } from "react";
+
 import styles from "./passwordLength.module.scss";
 
 // images
 import plus from "../../../../globals/images/plus.svg";
 import minus from "../../../../globals/images/minus.svg";
 
-const passwordLength = () => {
+const PasswordLength = () => {
+  const [progresSlider, setProgresSlider] = useState(13);
   return (
-    <div className={styles.password_length_full} role="group">
-      <input className={styles.password_length_input} type="text" name="fname" />
-      <div className={styles.password_length_buttonsAndSlider} role="group">
-        <button className={styles.password_length_button}>
-          <img className={styles.password_length_image} alt="button-minus" src={minus} />
-        </button>
-        <input className={styles.password_length_slider} type="range" min="1" max="50" value="13" />
-        <button className={styles.password_length_button}>
-          <img className={styles.password_length_image} alt="button-plus" src={plus} />
-        </button>
+    <>
+      <p className={styles.password_length_title}>Password length:</p>
+      <div className={styles.password_length_full} role="group">
+        <input className={styles.password_length_input} value={progresSlider} type="text" name="fname" />
+        <div className={styles.password_length_buttonsAndSlider} role="group">
+          <button className={styles.password_length_button}>
+            <img className={styles.password_length_image} alt="button-minus" src={minus} />
+          </button>
+          <div className={styles.password_length_slider_group}>
+            <input
+              className={styles.password_length_slider}
+              onChange={(e) => setProgresSlider(e.target.valueAsNumber)}
+              type="range"
+              min="1"
+              max="50"
+              value={progresSlider}
+            />
+            <div style={{ width: `${progresSlider * 2}%` }} className={styles.progressSlider}></div>
+          </div>
+          <button className={styles.password_length_button}>
+            <img className={styles.password_length_image} alt="button-plus" src={plus} />
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default passwordLength;
+export default PasswordLength;
