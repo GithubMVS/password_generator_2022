60fd3187c7f436b3253d58d1e6e093eddc938c41
Matisse <matisse.van.steenbergen@student.howest.be>
2022-07-22 10:19:18 +0200

    made css for characters

--- src/components/core/passwordGenerator/charactersUsed/characters.tsx
+++ src/components/core/passwordGenerator/charactersUsed/characters.tsx
@@ -2,23 +2,31 @@ import styles from "./characters.module.scss";
 
 const characters = () => {
   return (
-    <div className={styles.password_characters_full} role="group">
-      <label className={styles.password_characters_text}>
-        <input className={styles.password_characters_inputBox} type="checkbox" value="lowercase" name="characters" /> abc
-      </label>
-      <label className={styles.password_characters_text}>
-        <input className={styles.password_characters_inputBox} type="checkbox" value="capital" name="characters" /> ABC
-      </label>
-      <label className={styles.password_characters_text}>
-        <input className={styles.password_characters_inputBox} type="checkbox" value="numbers" name="characters" /> 123
-      </label>
-      <label className={styles.password_characters_text}>
-        <input className={styles.password_characters_inputBox} type="checkbox" value="test" name="characters" /> @#$%
-      </label>
-      <label className={styles.password_characters_text}>
-        <input className={styles.password_characters_inputBox} type="checkbox" value="test" name="characters" /> {`;.<>~`}
-      </label>
-    </div>
+    <>
+      <p className={styles.password_characters_title}>Characters used:</p>
+      <div className={styles.password_characters_full} role="group">
+        <label className={styles.password_characters_text}>
+          <input className={styles.password_characters_inputBox} type="checkbox" value="lowercase" name="characters" />
+          <span className={styles.password_characters_charactersUsed}>abc</span>
+        </label>
+        <label className={styles.password_characters_text}>
+          <input className={styles.password_characters_inputBox} type="checkbox" value="capital" name="characters" />
+          <span className={styles.password_characters_charactersUsed}>ABC</span>
+        </label>
+        <label className={styles.password_characters_text}>
+          <input className={styles.password_characters_inputBox} type="checkbox" value="numbers" name="characters" />
+          <span className={styles.password_characters_charactersUsed}>123</span>
+        </label>
+        <label className={styles.password_characters_text}>
+          <input className={styles.password_characters_inputBox} type="checkbox" value="test" name="characters" />
+          <span className={styles.password_characters_charactersUsed}>@#$%</span>
+        </label>
+        <label className={styles.password_characters_text}>
+          <input className={styles.password_characters_inputBox} type="checkbox" value="test" name="characters" />
+          <span className={styles.password_characters_charactersUsed}>{`;.<>~`}</span>
+        </label>
+      </div>
+    </>
   );
 };
 
